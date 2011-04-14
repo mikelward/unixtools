adb2307ad4ae31f15d4fbee596f929306ad07c07
Mikel Ward <mikel@mikelward.com>
2011-04-14 20:11:21 +1000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,3 @@
+CC=c99
+CFLAGS=-Wall -Werror
+#  vim: set ts=4 sw=4 tw=0 noet:
---
+++ ls2.c
@@ -0,0 +1,81 @@
+#define _BSD_SOURCE             /* for strdup() */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <assert.h>
+#include <dirent.h>
+#include <errno.h>
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int strcmpforqsort(const void *a, const void *b)
+{
+    const char *sa = *(char **)a;
+    const char *sb = *(char **)b;
+
+    //return strcmp(sa, sb);
+    return strcoll(sa, sb);
+}
+
+void ls(const char *root)
+{
+    struct stat buf;
+
+    errno = 0;
+    if (stat(root, &buf) == -1) {
+        fprintf(stderr, "ls2: cannot access %s: %s\n", root, strerror(errno));
+        return;
+    }
+
+    if (S_ISDIR(buf.st_mode)) {
+        errno = 0;
+        DIR *pdir = opendir(root);
+        if (pdir) {
+            char **names;
+            int namesmax = 0;
+            int i = 0;
+            struct dirent *pde;
+            while ((pde = readdir(pdir))) {
+                if (i == namesmax) {
+                    char **newnames = realloc(names, (namesmax+=256)*sizeof(*newnames));
+                    if (!newnames) {
+                        fprintf(stderr, "Error allocating memory\n");
+                        free(names);
+                        return;
+                    }
+                    names = newnames;
+                }
+                names[i++] = strdup(pde->d_name);
+            }
+
+            printf("%d files\n", i);
+            qsort(names, i, sizeof(char *), strcmpforqsort);
+            for (int j = 0; j < i; j++) {
+                if (strchr(names[j], '.') == names[j]) {
+                    /* skip dot files by default */
+                    continue;
+                }
+                printf("%s\n", names[j]);
+            }
+        }
+    }
+    else {
+        printf("%s\n", root);
+    }
+}
+
+int main(int argc, const char *argv[])
+{
+    setlocale(LC_ALL, "");
+
+    for (int i = 1; i < argc; i++) {
+        ls(argv[i]);
+    }
+
+    return 0;
+}
+
+/* vim: set ts=4 sw=4 tw=0 et:*/
