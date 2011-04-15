7ff658ccc17979930221d3656cedf8e947b40e44
Mikel Ward <mikel@mikelward.com>
2011-04-15 15:56:25 +1000

    Basics of ls -s now implemented (does not yet print totals).

--- ls2.c
+++ ls2.c
@@ -9,25 +9,30 @@
 #define _BSD_SOURCE             /* for strdup() */
 
 #include <sys/types.h>
+#include <sys/param.h>          /* for DEV_BSIZE */
 #include <sys/stat.h>
 #include <assert.h>
 #include <dirent.h>
 #include <errno.h>
 #include <getopt.h>             /* for getopt() */
 #include <locale.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
 struct options {
-    int (*sort)(const void *, const void *);
     int all : 1;
     int directory : 1;
+    int size : 1;
+    int blocksize;
+    int (*sort)(const void *, const void *);
 };
 
 struct file {
-    const char *filename;
+    const char *path;       /* full path to the file */
+    const char *filename;   /* path to the file without leading directories */
     struct stat *psb;
 };
 
@@ -42,6 +47,48 @@ int sortbyname(const void *a, const void *b)
     return strcoll(namea, nameb);
 }
 
+/* naive implementation; not benchmarked */
+/*******************************
+ * LAST ARGUMENT MUST BE NULL  *
+ * to signify end of arguments *
+ ******************************/
+char *strconcat(const char *s1, ...)
+{
+    int len, newlen;
+    char *ret, *rettmp;
+    va_list ap;
+
+    va_start(ap, s1);
+
+    const char *s;
+    ret = NULL;
+    len = 0;
+    for (s = s1;
+         s != NULL;
+         s = va_arg(ap, const char *)) {
+         newlen = len + strlen(s);
+         rettmp = realloc(ret, newlen+1);
+         if (!rettmp) {
+             fprintf(stderr, "Error realloc'ing string\n");
+             free(ret);
+             return NULL;
+         }
+         ret = rettmp;
+         strcpy(ret+len, s);
+         len = newlen;
+    }
+    return ret;
+}
+
+/*
+ * makepath("/dir", "file") => "/dir/file"
+ */
+char *makepath(const char *dirname, const char *filename)
+{
+    return strconcat(dirname, "/", filename, NULL);
+}
+
+
 /*
  * return a new struct file pointer with information about "path"
  *
@@ -52,21 +99,26 @@ int sortbyname(const void *a, const void *b)
  * if "wantstat" is false (0) or stat() fails, the psb member will be NULL
  * callers should check for this condition before using psb
  */
-struct file *getfile(const char *path, int wantstat)
+struct file *getfile(const char *dirname, const char *filename, int wantstat)
 {
     struct file *pfile = malloc(sizeof *pfile);
-    char *pathcopy = strdup(path);
-    pfile->filename = pathcopy;
+    if (!pfile) {
+        return NULL;
+    }
+
+    pfile->path = makepath(dirname, filename);
+    pfile->filename = strdup(filename);
     pfile->psb = NULL;
 
     if (wantstat) {
         struct stat *psb = malloc(sizeof *psb);
         if (psb) {
-            if (stat(path, psb) == 0) {
+            if (stat(pfile->path, psb) == 0) {
                 pfile->psb = psb;
             }
             else {
-                fprintf(stderr, "Error getting file status for %s\n", path);
+                fprintf(stderr, "Error getting file status for %s: %s\n",
+                        pfile->path, strerror(errno));
             }
         }
     }
@@ -80,7 +132,7 @@ struct file *getfile(const char *path, int wantstat)
  */
 int needfilestat(struct options *poptions)
 {
-    return 0;
+    return poptions->size;
 }
 
 /*
@@ -105,6 +157,22 @@ int wantthisfile(struct file *fp, struct options *poptions)
 
 void listfile(struct file *fp, struct options *poptions)
 {
+    if (poptions->size) {
+        /* yep, a file that's only using one block gets printed as size "0"!
+         * that's what ls does */
+        unsigned long size;
+        /* contortions to attempt to avoid overflow */
+        /* in both cases we want size = blocks * BSIZE / blocksize */
+        /* and in both cases we have to have larger / smaller
+         * to avoid truncating the answer down to zero */
+        if (DEV_BSIZE > poptions->blocksize) {
+            size = fp->psb->st_blocks * (DEV_BSIZE / poptions->blocksize);
+        } else {
+            size = fp->psb->st_blocks / (poptions->blocksize / DEV_BSIZE);
+        }
+        printf("%6lu ", size); 
+    }
+
     printf("%s\n", fp->filename);
 }
 
@@ -139,7 +207,7 @@ void listdir(struct file *fp, struct options *poptions)
                 }
                 files = newfiles;
             }
-            struct file *file = getfile(pde->d_name, wantstat);
+            struct file *file = getfile(dirname, pde->d_name, wantstat);
             if (!file) {
                 fprintf(stderr, "Error getting file\n");
                 continue;
@@ -166,13 +234,13 @@ void listdir(struct file *fp, struct options *poptions)
 /*
  * called for each file specified on the command line
  *
- * by default, if "path" is a directory, its contents will be listed
+ * by default, if "file" is a directory, its contents will be listed
  * the -d flag causes only the directory itself to be listed
  */
-void ls(const char *path, struct options *poptions)
+void ls(const char *file, struct options *poptions)
 {
     int wantdirstat = needdirstat(poptions);
-    struct file *fp = getfile(path, wantdirstat);
+    struct file *fp = getfile("", file, wantdirstat);
     if (!fp) {
         return;
     }
@@ -198,18 +266,23 @@ int main(int argc, char *argv[])
 
     struct options options;
     options.all = 0;
+    options.blocksize = 1024;       /* bytes per block */
     options.directory = 0;
+    options.size = 0;
     options.sort = &sortbyname;
 
     int option;
     opterr = 0;
-    while ((option = getopt(argc, argv, ":ad")) != -1) {
+    while ((option = getopt(argc, argv, ":ads")) != -1) {
         switch (option) {
         case 'a':
             options.all = 1;
             break;
         case 'd':
             options.directory = 1;
+            break;
+        case 's':
+            options.size = 1;
             break;
         case ':':
             fprintf(stderr, "Missing argument to -%c\n", optopt);
