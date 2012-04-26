79d544f31b134f3deb104933dc3a12a123daa047
Mikel Ward <mikel@mikelward.com>
2012-04-25 18:44:55 -0700

    Move Options into options.h.  Move Colors into display.h.

--- display.h
+++ display.h
@@ -3,6 +3,21 @@
 
 #include "list.h"
 
+/* terminal escape sequences for -G and -K flags
+ * not currently customizable
+ * see terminfo(3) for details */
+typedef struct colors {
+    char *black;
+    char *red;
+    char *green;
+    char *yellow;
+    char *blue;
+    char *magenta;
+    char *cyan;
+    char *white;
+    char *none;                     /* escape sequence to go back to default color */
+} Colors;
+
 typedef List StringList;
 
 void printacross(StringList *list, int stringwidth, int screenwidth);
--- l.c
+++ l.c
@@ -47,63 +47,7 @@
 #include "file.h"
 #include "list.h"
 #include "logging.h"
-
-/* terminal escape sequences for -G and -K flags
- * not currently customizable
- * see terminfo(3) for details */
-typedef struct colors {
-    char *black;
-    char *red;
-    char *green;
-    char *yellow;
-    char *blue;
-    char *magenta;
-    char *cyan;
-    char *white;
-    char *none;                     /* escape sequence to go back to default color */
-} Colors;
-
-/* defaults should be the first element */
-enum display { DISPLAY_ONE_PER_LINE, DISPLAY_IN_COLUMNS, DISPLAY_IN_ROWS };
-enum escape { ESCAPE_NONE, ESCAPE_QUESTION, ESCAPE_C };
-enum flags { FLAGS_NONE, FLAGS_NORMAL, FLAGS_OLD };
-enum timetype { TIME_MTIME, TIME_CTIME, TIME_ATIME, TIME_BTIME };
-enum sorttype { SORT_BY_NAME, SORT_BY_TIME, SORT_UNSORTED };
-
-/* all the command line options */
-/* defaults should usually be 0 */
-typedef struct options {
-    int all : 1;                    /* 1 = also print hidden files */
-    int bytes : 1;                  /* 1 = print file size in bytes */
-    int directory : 1;              /* 1 = print directory name rather than contents */
-    int dirsonly : 1;               /* 1 = don't print regular files */
-    int dirtotals : 1;              /* 1 = print directory size totals */
-    int inode : 1;                  /* 1 = print the inode number */
-    int linkcount : 1;              /* 1 = print number of hard links */
-    int showlink : 1;               /* 1 = show link -> target in name field (max. 1 link) */
-    int showlinks : 1;              /* 1 = show link -> target in name field (resolve all links) */
-    int targetinfo : 1;             /* 1 = field info is based on symlink target */
-    enum escape escape;             /*     how to handle non-printable characters */
-    enum flags flags;               /*     print file "flags" */
-    int group : 1;                  /* display the groupname of the file's group */
-    int modes : 1;                  /* displays file modes, e.g. -rwxr-xr-x */
-    int numeric : 1;                /* 1 = display numeric uid and gid instead of names */
-    int owner : 1;                  /* display the username of the file's owner */
-    int perms : 1;                  /* display rwx modes for current user */
-    int size : 1;                   /* 1 = print file size in blocks */
-    int datetime : 1;               /* 1 = print the file's modification date and time */
-    int reverse : 1;                /* 0 = forwards, 1 = reverse1 */
-    int color : 1;                  /* 1 = colorize file and directory names */
-    int blocksize;                  /* units for -s option */
-    time_t now;                     /* current time - for determining date/time format */
-    short displaymode;              /* one-per-line, columns, rows, etc. */ 
-    short screenwidth;              /* how wide the screen is, 0 if unknown */
-    Colors *pcolors;                /* the colors to use */
-    file_compare_function compare;  /* determines sort order */
-    const char *timeformat;         /* custom time format for -T */
-    enum timetype timetype;         /* which time to show (mtime, ctime, etc.) */
-    enum sorttype sorttype;         /* how to sort */
-} Options;
+#include "options.h"
 
 typedef List FileList;              /* list of files */
 typedef List FieldList;             /* list of fields for a single file */
