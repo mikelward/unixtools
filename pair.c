d1f06d817b00caa4e4ffcfc69c5c966ec81812b8
Mikel Ward <mikel@mikelward.com>
2012-04-28 11:25:09 -0700

    Fix indentation in pair.[ch].

--- pair.c
+++ pair.c
@@ -1,4 +1,4 @@
-#define _XOPEN_SOURCE 600		/* for strdup() */
+#define _XOPEN_SOURCE 600       /* for strdup() */
 
 #include <stdlib.h>
 #include <string.h>
@@ -6,73 +6,73 @@
 #include "logging.h"
 
 typedef struct pair {
-	int key;
-	char *value;
+    int key;
+    char *value;
 } Pair;
 
 Pair *newpair(int key, char *value)
 {
-	Pair *pair = malloc(sizeof(*pair));
-	if (!pair) {
-		errorf("Out of memory?\n");
-		return NULL;
-	}
+    Pair *pair = malloc(sizeof(*pair));
+    if (!pair) {
+        errorf("Out of memory?\n");
+        return NULL;
+    }
 
-	pair->key = key;
-	if (value) {
-		pair->value = strdup(value);
-		if (!pair->value) {
-			errorf("Out of memory?\n");
-			free(pair);
-			return NULL;
-		}
-	}
-	
-	return pair;
+    pair->key = key;
+    if (value) {
+        pair->value = strdup(value);
+        if (!pair->value) {
+            errorf("Out of memory?\n");
+            free(pair);
+            return NULL;
+        }
+    }
+
+    return pair;
 }
 
 void freepair(Pair *pair)
 {
-	if (!pair) return;
-	free(pair->value);
-	free(pair);
+    if (!pair) return;
+    free(pair->value);
+    free(pair);
 }
 
 int getkey(Pair *pair)
 {
-	if (!pair) {
-		errorf("pair is NULL\n");
-		return 0;
-	}
+    if (!pair) {
+        errorf("pair is NULL\n");
+        return 0;
+    }
 
-	return pair->key;
+    return pair->key;
 }
 
 char *getvalue(Pair *pair)
 {
-	if (!pair) {
-		errorf("pair is NULL\n");
-		return 0;
-	}
+    if (!pair) {
+        errorf("pair is NULL\n");
+        return 0;
+    }
 
-	return pair->value;
+    return pair->value;
 }
 
 void setvalue(Pair *pair, char *value)
 {
-	if (!pair) {
-		errorf("pair is NULL\n");
-		return;
-	}
+    if (!pair) {
+        errorf("pair is NULL\n");
+        return;
+    }
 
-	free(pair->value);
-	if (value) {
-		pair->value = strdup(value);
-		if (!pair->value) {
-			errorf("Out of memory?\n");
-			return;
-		}
-	} else {
-		pair->value = value;
-	}
+    free(pair->value);
+    if (value) {
+        pair->value = strdup(value);
+        if (!pair->value) {
+            errorf("Out of memory?\n");
+            return;
+        }
+    } else {
+        pair->value = value;
+    }
 }
