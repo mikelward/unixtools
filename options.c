mikelward/unixtools

commit 4ea88200b47892d54a45e1e59b0754b0d3b281e0

Option flags are now single bit bools rather than ints.
This more closely matches their use, and also makes them unsigned rather
than signed, which might be useful for using memset/calloc to set their
defaults to false in future.

options.c
@@ -38,31 +38,31 @@ Options *newoptions(void)
 void setdefaults(Options *options)
 {
     /* TODO use memset? */
-    options->all = 0;
+    options->all = false;
     options->blocksize = 1024;
-    options->bytes = 0;
-    options->color = 0;
-    options->datetime = 0;
-    options->directory = 0;
-    options->dirsonly = 0;
-    options->dirtotals = 0;
+    options->bytes = false;
+    options->color = false;
+    options->datetime = false;
+    options->directory = false;
+    options->dirsonly = false;
+    options->dirtotals = false;
     options->displaymode = DISPLAY_ONE_PER_LINE;
     options->escape = ESCAPE_NONE;
     options->flags = FLAGS_NONE;
-    options->group = 0;
-    options->inode = 0;
-    options->linkcount = 0;
-    options->modes = 0;
-    options->numeric = 0;
-    options->owner = 0;
-    options->perms = 0;
-    options->recursive = 0;
-    options->reverse = 0;
-    options->showlink = 0;
-    options->showlinks = 0;
-    options->size = 0;
+    options->group = false;
+    options->inode = false;
+    options->linkcount = false;
+    options->modes = false;
+    options->numeric = false;
+    options->owner = false;
+    options->perms = false;
+    options->recursive = false;
+    options->reverse = false;
+    options->showlink = false;
+    options->showlinks = false;
+    options->size = false;
     options->sorttype = SORT_BY_NAME;
-    options->targetinfo = 0;
+    options->targetinfo = false;
     options->timetype = TIME_MTIME;
 
     options->compare = NULL;
@@ -114,14 +114,14 @@ int setoptions(Options *options, int argc, char **argv)
             /* maybe print ACLs?  or -a without "." and ".."? */
             break;
         case 'a':
-            options->all = 1;
+            options->all = true;
             break;
         case 'B':
-            options->bytes = 1;
+            options->bytes = true;
             break;
         case 'b':
             /* GNU ls uses this for ESCAPE_C, but we use -e = escape */
-            options->bytes = 1;
+            options->bytes = true;
             break;
         case 'C':
             options->displaymode = DISPLAY_IN_COLUMNS;
@@ -131,10 +131,10 @@ int setoptions(Options *options, int argc, char **argv)
             /* this interacts with other options-> see below */
             break;
         case 'D':
-            options->dirsonly = 1;
+            options->dirsonly = true;
             break;
         case 'd':
-            options->directory = 1;
+            options->directory = true;
             break;
         case 'E':
             options->escape = ESCAPE_NONE;
@@ -150,15 +150,15 @@ int setoptions(Options *options, int argc, char **argv)
             break;
         case 'G':
             /* for compatibility with FreeBSD */
-            options->color = 1;
+            options->color = true;
             break;
         case 'g':
             /* hopefully saner than legacy ls */
-            options->group = 1;
+            options->group = true;
             break;
         case 'K':
             /* K = "kolor", somewhat mnemonic and unused in GNU ls */
-            options->color = 1;
+            options->color = true;
             break;
         case 'k':
             options->blocksize = 1024;
@@ -167,67 +167,67 @@ int setoptions(Options *options, int argc, char **argv)
             options->timeformat = "%Y-%m-%d %H:%M:%S";
             break;
         case 'i':
-            options->inode = 1;
+            options->inode = true;
             break;
         case 'L':
-            options->showlinks = 1;
-            options->targetinfo = 1;
+            options->showlinks = true;
+            options->targetinfo = true;
             break;
         case 'l':
-            options->modes = 1;
-            options->linkcount = 1;
-            options->owner = 1;
-            options->group = 1;
-            options->bytes = 1;
-            options->datetime = 1;
-            options->showlink = 1;
+            options->modes = true;
+            options->linkcount = true;
+            options->owner = true;
+            options->group = true;
+            options->bytes = true;
+            options->datetime = true;
+            options->showlink = true;
             options->displaymode = DISPLAY_ONE_PER_LINE;
-            options->dirtotals = 1;
+            options->dirtotals = true;
             break;
         case 'M':
             /* might change this to blocksize=1048576 later */
-            options->modes = 1;
+            options->modes = true;
             break;
         case 'm':
             /* conflicts with legacy ls "streams" mode */
-            options->modes = 1;
+            options->modes = true;
             break;
         case 'N':
-            options->linkcount = 1;
+            options->linkcount = true;
             break;
         case 'n':
-            options->numeric = 1;
+            options->numeric = true;
             break;
         case 'O':
             options->flags = FLAGS_OLD;
             break;
         case 'o':
-            options->owner = 1;
+            options->owner = true;
             break;
         case 'P':
             /* reserved for physical mode (don't follow symlinks) */
             break;
         case 'p':
-            options->perms = 1;
+            options->perms = false;
             break;
         case 'q':
             options->escape = ESCAPE_QUESTION;
             break;
         case 'R':
-            options->recursive = 1;
+            options->recursive = false;
             break;
         case 'r':
-            options->reverse = 1;
+            options->reverse = false;
             break;
         case 'S':
             options->sorttype = SORT_BY_SIZE;
             break;
         case 's':
-            options->size = 1;
-            options->dirtotals = 1;
+            options->size = false;
+            options->dirtotals = false;
             break;
         case 'T':
-            options->datetime = 1;
+            options->datetime = false;
             break;
         case 't':
             options->sorttype = SORT_BY_TIME;

options.h
@@ -2,6 +2,7 @@
 #define OPTIONS_H
 
 #include <sys/types.h>
+#include <stdbool.h>
 
 #include "buf.h"
 #include "display.h"
@@ -20,31 +21,31 @@ enum sorttype { SORT_BY_NAME, SORT_BY_TIME, SORT_BY_SIZE, SORT_UNSORTED };
 /* all the command line options */
 /* defaults should usually be 0 */
 typedef struct options {
-    int all : 1;                    /* 1 = also print hidden files */
-    int blocksize;                  /* units for -s option */
-    int bytes : 1;                  /* 1 = print file size in bytes */
-    int color : 1;                  /* 1 = colorize file and directory names */
-    int datetime : 1;               /* 1 = print the file's modification date and time */
-    int directory : 1;              /* 1 = print directory name rather than contents */
-    int dirsonly : 1;               /* 1 = don't print regular files */
-    int dirtotals : 1;              /* 1 = print directory size totals */
+    bool all : 1;                   /* true = also show hidden files */
+    bool blocksize;                 /* units for -s option */
+    bool bytes : 1;                 /* true = show file size in bytes */
+    bool color : 1;                 /* true = colorize file and directory names */
+    bool datetime : 1;              /* true = show the file's modification date and time */
+    bool directory : 1;             /* true = show directory name rather than contents */
+    bool dirsonly : 1;              /* true = only list directories, not regular files */
+    bool dirtotals : 1;             /* true = show directory size totals */
     enum display displaymode;       /* one-per-line, columns, rows, etc. */ 
     enum escape escape;             /*     how to handle non-printable characters */
-    enum flags flags;               /*     print file "flags" */
-    int group : 1;                  /* display the groupname of the file's group */
-    int inode : 1;                  /* 1 = print the inode number */
-    int linkcount : 1;              /* 1 = print number of hard links */
-    int modes : 1;                  /* displays file modes, e.g. -rwxr-xr-x */
-    int numeric : 1;                /* 1 = display numeric uid and gid instead of names */
-    int owner : 1;                  /* display the username of the file's owner */
-    int perms : 1;                  /* display rwx modes for current user */
-    int recursive : 1;              /* display subdirectories recursively */
-    int reverse : 1;                /* 0 = forwards, 1 = reverse1 */
-    int showlink : 1;               /* 1 = show link -> target in name field (max. 1 link) */
-    int showlinks : 1;              /* 1 = show link -> target in name field (resolve all links) */
-    int size : 1;                   /* 1 = print file size in blocks */    
+    enum flags flags;               /*     show file "flags" */
+    bool group : 1;                 /* true = show the file's group */
+    bool inode : 1;                 /* true = show the inode number */
+    bool linkcount : 1;             /* true = show number of hard links */
+    bool modes : 1;                 /* true = show the file's modes, e.g. -rwxr-xr-x */
+    bool numeric : 1;               /* true = show uid and gid instead of username and groupname */
+    bool owner : 1;                 /* true = show the file's owner */
+    bool perms : 1;                 /* true = show permissions for the current user, e.g. rwx */
+    bool recursive : 1;             /* true = after listing a directory, list its subdirectories recursively */
+    bool reverse : 1;               /* true = sort oldest to newest (or smallest to largest with -S option) */
+    bool showlink : 1;              /* true = show link -> target in name field (max. 1 link) */
+    bool showlinks : 1;             /* true = show link -> target in name field (resolve all links) */
+    bool size : 1;                  /* true = show file size in blocks */
     enum sorttype sorttype;         /* how to sort */
-    int targetinfo : 1;             /* 1 = field info is based on symlink target */
+    bool targetinfo : 1;            /* true = field info is based on symlink target */
     enum timetype timetype;         /* which time to show (mtime, ctime, etc.) */
 
     /* these are more like global state variables than options */

commit da6c77cf3c273e820afb0d9d742a21d957315bc5

Fix -R, -r, -s, and -T flags (broken by 4ea88200).

options.c
@@ -208,26 +208,26 @@ int setoptions(Options *options, int argc, char **argv)
             /* reserved for physical mode (don't follow symlinks) */
             break;
         case 'p':
-            options->perms = false;
+            options->perms = true;
             break;
         case 'q':
             options->escape = ESCAPE_QUESTION;
             break;
         case 'R':
-            options->recursive = false;
+            options->recursive = true;
             break;
         case 'r':
-            options->reverse = false;
+            options->reverse = true;
             break;
         case 'S':
             options->sorttype = SORT_BY_SIZE;
             break;
         case 's':
-            options->size = false;
-            options->dirtotals = false;
+            options->size = true;
+            options->dirtotals = true;
             break;
         case 'T':
-            options->datetime = false;
+            options->datetime = true;
             break;
         case 't':
             options->sorttype = SORT_BY_TIME;